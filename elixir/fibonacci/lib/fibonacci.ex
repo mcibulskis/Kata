c461434c15a636d63989365d305afd5366cb2fce
Michael Cibulskis <Michael.Cibulskis@noackexpected.com>
2020-07-21 20:51:33 -0400

    refactored to include the n+1 case

--- elixir/fibonacci/lib/fibonacci.ex
+++ elixir/fibonacci/lib/fibonacci.ex
@@ -8,11 +8,27 @@ defmodule Fibonacci do
 
   ## Examples
 
-      iex> Fibonacci.sequence() |> Enum.take(2)
-      [0, 1]
+      iex> Fibonacci.sequence() |> Enum.take(10)
+      [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
   """
   def sequence do
-    Stream.cycle([0, 1])
+    Stream.resource(
+      fn -> [] end,
+      &next_fib/1,
+      fn fibs -> fibs end
+    )
+  end
+
+  defp next_fib([]) do
+    {[0], [0]}
+  end
+
+  defp next_fib([0]) do
+    {[1], [0, 1]}
+  end
+
+  defp next_fib([prev1, prev2]) do
+    {[prev1 + prev2], [prev2, prev1 + prev2]}
   end
 end
--- elixir/fibonacci/test/fibonacci_test.exs
+++ elixir/fibonacci/test/fibonacci_test.exs
@@ -9,4 +9,8 @@ defmodule FibonacciTest do
   test "returns 1 as the second element" do
     assert Fibonacci.sequence() |> Enum.take(2) |> tl |> hd == 1
   end
+
+  test "subsequent elements are the sum of the previous two elements" do
+    assert Fibonacci.sequence() |> Enum.take(6) == [0, 1, 1, 2, 3, 5]
+  end
 end
