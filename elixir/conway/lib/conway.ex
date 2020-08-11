d0c7b67773736a25bf9187ba2e5d48792929f009
Michael Cibulskis <Michael.Cibulskis@noackexpected.com>
2020-08-10 21:11:37 -0400

    initial commit for Conway's game of life

---
+++ elixir/conway/lib/conway.ex
@@ -0,0 +1,18 @@
+defmodule Conway do
+  @moduledoc """
+  Documentation for `Conway`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Conway.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
